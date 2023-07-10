51deeeeb0e86ef46386cb16c23e7c399b9366196
claudiaacchurch <claudiachurch00@gmail.com>
2023-07-10 15:53:32 +0100

    Initial

--- app.py
+++ app.py
@@ -16,12 +16,51 @@ app = Flask(__name__)
 def get_emoji():
     return ":)"
 
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args['name']
+    return f"Hello {name}"
+
+@app.route('/goodbye', methods=['POST'])
+def goodbye():
+    name = request.form['name'] # The value is 'Alice'
+
+    # Send back a fond farewell with the name
+    return f"Goodbye {name}!"
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    message = request.form['message']
+    return f'Thanks {name}, you sent this message: "{message}"'
+
+@app.route('/wave', methods=['GET'])
+def wave():
+    NAME = request.args['name']
+    return f"I am waving at {NAME}"
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
 from example_routes import apply_example_routes
 apply_example_routes(app)
 
 # == End Example Code ==
+
+@app.route('/names', methods=['GET'])
+def names():
+    name = request.args['add']
+    split_names = name.split(',')
+    if len(split_names) == 1:
+        names_list = "Julia, Alice, Karim"
+        return f"{names_list}, {name}"
+    else:
+        names_list = "Julia,Alice,Karim"
+        split_list = names_list.split(",")
+        for name in split_names:
+            split_list.append(name)
+        print(split_list)
+        sort_names = sorted(split_list)
+        print(sort_names)
+        return ", ".join(sort_names)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,49 @@ def test_get_emoji(web_client):
     assert response.data.decode("utf-8") == ":)"
 
 # === End Example Code ===
+
+"""
+When: I make a GET request to /
+Then: I should get a 200 response
+"""
+def test_get_wave(web_client):
+    # We'll simulate sending a GET request to /wave?name=Dana
+    # This returns a response object we can test against.
+    response = web_client.get('/wave?name=Dana')
+
+    # Assert that the status code was 200 (OK)
+    assert response.status_code == 200
+
+    # Assert that the data returned was the right string
+    assert response.data.decode('utf-8') == 'I am waving at Dana'
+
+"""
+When: I make a POST request to /submit
+And: I send a name and message as body parameters
+Then: I should get a 200 response with the right content
+"""
+def test_post_submit(web_client):
+    # We'll simulate sending a POST request to /submit with a name and message
+    # This returns a response object we can test against.
+    response = web_client.post('/submit', data={'name': 'Dana', 'message': 'Hello'})
+
+    # Assert that the status code was 200 (OK)
+    assert response.status_code == 200
+
+    # Assert that the data returned was the right string
+    assert response.data.decode('utf-8') == 'Thanks Dana, you sent this message: "Hello"'
+
+def test_get_names_plus_eddie(web_client):
+    response = web_client.get('/names?add=Eddie')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Julia, Alice, Karim, Eddie"
+
+def test_get_names_plus_tom(web_client):
+    response = web_client.get('/names?add=Tom')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Julia, Alice, Karim, Tom"
+
+def test_get_sorted_names_when_multiple_names_added(web_client):
+    response = web_client.get('/names?add=Eddie,Tom')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Alice, Eddie, Julia, Karim, Tom"
